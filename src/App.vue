17e5dbe0229c87f021c80d9cf498b6d0696d5cda
nazneen1022 <nazneen1022@gmail.com>
2020-10-31 15:43:08 +0100

    initial setup with navigation links

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/Categories">Categories</router-link> | 
+      <router-link to="/Random">Random</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Home' as string,
+  components:{
+
+  }
+})
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  text-align:left;
+  padding:20px;
+  background: #2c3e50;
+
+  a {
+    padding:50px;
+    font-weight: bold;
+    color: white;
+
+    &.router-link-exact-active {
+      color: orangered;
+    }
+  }
+}
+</style>
